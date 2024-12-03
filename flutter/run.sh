88c4c17c0fe4e277e164cd13b4c6d81f5e6887af
zzyi <uniminio@qq.com>
2024-12-03 18:05:24 +0800

    worker: 更改工作流3

--- flutter/run.sh
+++ flutter/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cargo add cargo-platform --version 0.1.8
+cargo add cargo-platform@=0.1.8
 cargo install flutter_rust_bridge_codegen --version 1.80.1 --features uuid
 flutter pub get
 ~/.cargo/bin/flutter_rust_bridge_codegen --rust-input ../src/flutter_ffi.rs --dart-output ./lib/generated_bridge.dart --c-output ./macos/Runner/bridge_generated.h
